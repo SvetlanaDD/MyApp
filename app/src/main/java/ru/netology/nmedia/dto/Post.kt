ed380579e0b278bb5b626197e642778b025f3add
SvetlanaDD <asweet.lana@gmail.com>
2023-03-16 14:16:34 +0300

    Add Avatar and Attachment (IMAGE)

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -3,9 +3,16 @@ package ru.netology.nmedia.dto
 data class Post(
     val id: Long,
     val author: String,
+    val authorAvatar: String,
     val content: String,
     val published: String,
     val likedByMe: Boolean,
     var likes: Int = 0,
+    val attachment: Attachment?
 )
 
+data class Attachment (
+    val url: String,
+    val description: String,
+    val type: String
+        )
